b0cc7c1103fc43009473a206e24b7560d2343960
LittleChild <work_yuanyang@163.com>
2016-08-24 10:54:32 +0800

    去掉lodash

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var exec       = require('child_process').exec;
 var nodemailer = require('nodemailer');
 var cronJob    = require('cron').CronJob;
-var _          = require('lodash');
 var reasons    = require('./reasons').reasons;
 
 var job = new cronJob('00 50 09 * * 0-6', function(){
@@ -15,7 +14,7 @@ var job = new cronJob('00 50 09 * * 0-6', function(){
       to: 'leader-name@kuyun.com',
       bcc: 'CC-name@163.com',
       subject: '请假申请',
-      text: reasons[_.random(0, reasons.length-1)]
+      text: reasons[Math.floor(Math.random() * reasons.length)]
     },function(err){
       console.log(err, 'err');
     });
